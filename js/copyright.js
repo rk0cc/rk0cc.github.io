32f86d3e8fa809f74bd19f9ebac84225003eaa08
Cyrus Chan <cyruschan1212@gmail.com>
2024-06-04 19:07:40 +0800

    Update content

---
+++ js/copyright.js
@@ -0,0 +1,7 @@
+"use strict";
+
+(function () {
+    const placeholder = document.querySelector("footer > p.copy-right");
+
+    placeholder.innerHTML = "rk0cc \u00A9 2021 - " + new Date().getFullYear();
+})();
