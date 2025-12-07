b3d50d0d81aaee13d5e631760d43af53d91e1ad9
Cyrus Chan <cyruschan1212@gmail.com>
2025-12-07 16:36:33 +0800

    Renew webpage

--- js/copyright.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-(function () {
-    const placeholder = document.querySelector("footer > p.copy-right");
-
-    placeholder.innerHTML = "rk0cc \u00A9 2021 - " + new Date().getFullYear();
-})();
